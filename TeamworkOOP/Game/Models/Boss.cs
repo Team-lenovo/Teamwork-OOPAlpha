285fff96a6f016133079917fdc790c7e87e60ccb
didox2 <didox222@gmail.com>
2017-08-10 18:29:16 +0300

    Boss added

--- TeamworkOOP/Game/Models/Boss.cs
+++ TeamworkOOP/Game/Models/Boss.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcademyInvaders.Models.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace AcademyInvaders.Models
 {
-    public class Boss
+    public class Boss : Enemy
     {
+        private IList<IBullet> shootedBullets;
+        
+        public Boss(Position position, int health, Size? size, ConsoleColor color, int randomX) : base(position, health, size, color, randomX)
+        {
+            this.ShootedBullets = new List<IBullet>();
+        }
+
+        public IList<IBullet> ShootedBullets
+        {
+            get
+            {
+                return this.shootedBullets;
+            }
+            set
+            {
+                this.shootedBullets = value;
+            }
+        }
     }
 }
